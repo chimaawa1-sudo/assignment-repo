85afb0ecbf4b787f144df4fa0362cfea0f1dc2c7
chimaawa1 <chimaawa1@gmail.com>
2026-01-29 14:45:32 -0800

    ass

---
+++ script.js
@@ -0,0 +1,12 @@
+//write the following in a string or numbetrs...store them either with const or let
+//1...your name...age, city height,weight,country,address,gender and console log them
+const name = "Awa chima";
+let age = 18;
+let city = "Lagos";
+let height = 175;
+let weight = 70;
+const country = "Nigeria";
+let address = "38 sanusi street";
+const gender = "Male";
+
+console.log( age, city, height, weight, country, address, gender);
