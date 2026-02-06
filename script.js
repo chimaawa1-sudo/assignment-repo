93ba4c4f54a140809b5ed0e82bef474ba5b2cb23
chimaawa1 <chimaawa1@gmail.com>
2026-02-06 04:01:55 -0800

    uppsss

--- script.js
+++ script.js
@@ -25,18 +25,33 @@
 
 //assignment operators
 //write a program that calculates the area of a rectangle given its length and width using assignment operators.
-let length = 15;
-let width = 8;
-let area = length * width;
-console.log(area);
+// let length = 15;
+// let width = 8;
+// let area = length * width;
+// console.log(area);
 
-//write a program that calculates the area of a circle given its radius using assignment and arithmetic operators.
-let radius = 8;
-const pi = 3.14;
-let areaOfCircle = pi * radius * radius;
-console.log(areaOfCircle);
+// //write a program that calculates the area of a circle given its radius using assignment and arithmetic operators.
+// let radius = 8;
+// const pi = 3.14;
+// let areaOfCircle = pi * radius * radius;
+// console.log(areaOfCircle);
 
-//write a program that converts temperature from Celsius to Fahrenheit using assignment and arithmetic operators.
-let celsius = 28;
-let fahrenheit = (celsius * 9/5) + 32;
-console.log(fahrenheit);
+// //write a program that converts temperature from Celsius to Fahrenheit using assignment and arithmetic operators.
+// let celsius = 28;
+// let fahrenheit = (celsius * 9/5) + 32;
+// console.log(fahrenheit);
+
+//write a program that takes in a users age and outputs if the user is a child, teenager, adult.
+const age = 27;
+if (age < 13) {
+    console.log("the user is a child");
+} else if (age >= 13 && age < 20) {
+    console.log("the user is a teenager");
+} else {
+    console.log("the user is an adult");
+}
+
+//loop through the numbers 1 to 50
+for (let i = 1; i <= 50; i++) {
+    console.log(i);
+}
